708b6e73631ebb27647209ac78e00a2ac3e0d978
soeyusuke <s1240231@gmail.com>
2018-07-19 13:50:23 +0900

    [ADD] carousel template

--- template/carousel.go
+++ template/carousel.go
@@ -1,1 +1,39 @@
 package template
+
+import (
+	"errors"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+type Carousel struct {
+	Columns []*linebot.CarouselColumn
+}
+
+func NewColumns(btn []Buttons) (Carousel, error) {
+	if len(btn) > 10 {
+		return Carousel{}, errors.New("columns must not be more than 10 items")
+	}
+
+	var c Carousel
+	for _, v := range btn {
+		if len(v.Button) > 3 {
+			return Carousel{}, errors.New("button actions must not be more than 3 items")
+		}
+
+		c.Columns = append(c.Columns, &linebot.CarouselColumn{
+			ThumbnailImageURL: v.ImagePath,
+			Title:             v.Title,
+			Text:              v.SubTitle,
+			Actions:           v.Button,
+		})
+	}
+	return c, nil
+}
+
+func (c Carousel) CarouselTemplate() *linebot.TemplateMessage {
+	return linebot.NewTemplateMessage("carousel",
+		&linebot.CarouselTemplate{
+			Columns: c.Columns,
+		})
+}
